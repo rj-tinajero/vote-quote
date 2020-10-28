50eb9b9e402f9dfc08a4206f38345246f44811b2
Richard Tinajero <rj.tinajero92@gmail.com>
2020-10-27 19:53:29 -0700

    Updates

---
+++ precache-manifest.1a6053b0e7f15c3bb872f56716490f19.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "755bed70e4fe9f0fd82dcffd3323284d",
+    "url": "/votequote/index.html"
+  },
+  {
+    "revision": "23cae085cbfd4b196193",
+    "url": "/votequote/static/css/main.5f396199.chunk.css"
+  },
+  {
+    "revision": "e5ad978607858d911f8c",
+    "url": "/votequote/static/js/2.05225c0b.chunk.js"
+  },
+  {
+    "revision": "14bc303abaa4137dd915746d83d25dbc",
+    "url": "/votequote/static/js/2.05225c0b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "23cae085cbfd4b196193",
+    "url": "/votequote/static/js/main.65471d2d.chunk.js"
+  },
+  {
+    "revision": "c8dcf266ebd68ec4d597",
+    "url": "/votequote/static/js/runtime-main.560cd637.js"
+  }
+]);
